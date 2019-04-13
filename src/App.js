9467558ce64ac07293241ffa445ba8b075b4c04e
github username <github email address>
2019-04-13 17:38:00 -0400

    frontend set up

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state= {
+    departments: []
+  }
+  componentDidMount () {
+    fetch('http://localhost:3000/api/v1/departments', {mode: 'cors'})
+    .then(response => response.json())
+    .then(departments => console.log(departments))
+  }
   render() {
     return (
       <div className="App">
         <h1>Looking</h1>
+
       </div>
     );
   }
