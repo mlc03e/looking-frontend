6c23a317861f81af95a6032948ff05dd6586246f
github username <github email address>
2019-05-12 17:07:01 -0400

    hover drop-down

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
+  state= {
+    showWomen: false
+  }
 
+womenMouseOver=()=> {
+  this.setState({showWomen: true})
+}
+womenMouseLeave=()=> {
+  this.setState({showWomen: false})
+}
   render() {
     return (
       <div className= 'navBar'>
         <h1> Home </h1>
-        <h1> Womens </h1>
+        <div className='women'>
+          <h1 onMouseEnter= {this.womenMouseOver} onMouseLeave= {this.womenMouseLeave}> Womens </h1>
+          {this.state.showWomen && <ul><li>Shirts </li><li>Pants </li></ul>}
+        </div>
         <h1> Mens </h1>
         <h1> Accessories </h1>
       </div>
