332234a3ff4498553b9462625ae51a163c1e4ff5
github username <github email address>
2019-05-19 16:06:57 -0400

    mens categories

--- src/DepartmentsContainer.js
+++ src/DepartmentsContainer.js
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 class DepartmentsContainer extends Component {
 
   render() {
-    console.log(this.props.categories[0].map(c => c.name));
+    // console.log(this.props.categories[0].map(c => c.name));
 
     return (
       <div>
         <ul>
-          <li>{this.props.categories[0].map(c => `${c.name}`)}</li>
+          {this.props.categories.map(c => <li>`${c.name}`</li>)}
         </ul>
 
       </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,24 +3,30 @@ import DepartmentsContainer from './DepartmentsContainer'
 
 class Navbar extends Component {
   state= {
-    showWomen: false
+    showCategories: false
   }
 
-womenMouseOver=()=> {
-  this.setState({showWomen: true})
+categoriesMouseOver=()=> {
+  this.setState({showCategories: true})
 }
-womenMouseLeave=()=> {
-  this.setState({showWomen: false})
+categoriesMouseLeave=()=> {
+  this.setState({showCategories: false})
 }
   render() {
+    const categories= this.props.departments.map(d => d.categories)
+    console.log(categories);
     return (
       <div className= 'navBar'>
         <h1> Home </h1>
         <div className='women'>
-          <h1 onMouseEnter= {this.womenMouseOver} onMouseLeave= {this.womenMouseLeave}> Womens </h1>
-          {this.state.showWomen && <DepartmentsContainer categories={this.props.departments.map(d => d.categories)}/>}
+          <h1 onMouseEnter= {this.categoriesMouseOver} onMouseLeave= {this.categoriesMouseLeave}> Womens </h1>
+          {this.state.showCategories && <DepartmentsContainer categories={categories[0]}/>}
         </div>
-        <h1> Mens </h1>
+        <div className='men'>
+          <h1 onMouseEnter= {this.categoriesMouseOver} onMouseLeave= {this.categoriesMouseLeave}> Mens </h1>
+          {this.state.showCategories && <DepartmentsContainer categories={categories[1]}/>}
+        </div>
+
         <h1> Accessories </h1>
       </div>
     );
